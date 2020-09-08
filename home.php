2c81ffbc0d2b15417d2e21861d0299da3e6e5183
Daniel Janson <DanielAugustJanson@users.noreply.github.com>
2020-09-08 11:09:21 +0300

    esimene yleslaadimine

---
+++ home.php
@@ -0,0 +1,40 @@
+<?php
+$username = "Daniel Janson";
+$fulltimenow = date("d.m.Y H:i:s");
+$partofday = "lihtsalt aeg";
+$hournow = date("H");
+if($hournow <6){
+	$partofday = "uneaeg";
+} // Tingimuse algust ja loppu tahistatakse loogikasulgudega {}
+if($hournow >= 8 and $hournow <= 18){
+	$partofday = "kooli aeg";
+}
+?>
+
+
+<!-- Iga dokument algab dokumendi tyybi delkaratsiooniga -->
+<!DOCTYPE html>
+
+<!-- Iga veebilehe sisu jaab HTML sisse -->
+<html lang="et">
+
+<head>
+<!-- Siia jaavad asjad mis defineerivad lehekulge ja peamiselt jaab selle sisu meile nahtamatuks -->
+
+<!-- Defineerib mis tahemarke veebileht kasutab -->
+<meta charset="utf-8">
+
+<!-- Veebi pealkiri mida naeb selle tabis -->
+<title><?php echo $username; ?> loob lehekülge</title>
+
+</head>
+<body>
+  <h1>"Your name here"</h1>
+  <p>See on paragraaf õppetöö jaoks</p>
+  <p>Praegu on <?php echo $partofday; ?> </p>
+  <p>See leht on loodud Veebiprogrammeerimise kursusel 2020. aastal <a href="https://www.tlu.ee">Tallinna Ülikoolis </a> Digitehnoloogia Instituudis. </p>
+  <p>Lehekülg oli viimati värkendatud : <?php echo $fulltimenow; ?></p>
+
+<!-- HTMLis tahistatakse elemedi algus nool sulgudega ja lopetatakse pandel elemendi ette "/" -->
+</body>
+</html>
